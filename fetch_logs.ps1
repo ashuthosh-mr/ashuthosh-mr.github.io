310a58b9f0e1cb75801c70b95d5af39a3fecda2e
ashuthosh-mr <ashuthosh_mr@pm.me>
2026-08-05 12:32:12 +0530

    Remove temp log files

--- fetch_logs.ps1
+++
@@ -1,3 +0,0 @@
-$response = Invoke-WebRequest -Uri 'https://api.github.com/repos/ashuthosh-mr/ashuthosh-mr.github.io/actions/jobs/92227304893/logs'
-$lines = $response.Content -split "`n"
-$lines[-50..-1]
